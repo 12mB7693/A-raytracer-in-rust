cbf40b01a2d22632f6b05ff7519a808ea0b1fc7a
Your Name <you@example.com>
2025-07-29 21:43:22 +0200

    implement diffuse materials

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use rand::Rng;
 fn main() {
     let nx = 200*3;
     let ny = 100*3;
-    let ns = 100;
+    let ns = 20;
     //let first_line = "P3\n" + nx.to_string();
     //println!(first_line);
     let header = format!("P3\n{} {}\n255\n", nx.to_string(), ny.to_string());
@@ -34,6 +34,8 @@ fn main() {
                 //let secret_number = rand::thread_rng().gen_range(1..=100);
                 let x : f64 = rng.random();
                 let y : f64 = rng.random();
+                // let x = 0.0;
+                // let y = 0.0;
                 let u = (i as f64 + x)/nx as f64;
                 let v = (j as f64 + y)/ny as f64;
 
@@ -65,6 +67,7 @@ fn main() {
                 // }
             }
             col = col * (1.0 / ns as f64) ;
+            col = Vec3(col.r().sqrt(), col.g().sqrt(), col.b().sqrt()); // gamma correction
             let ir = (255.99*col.r()) as u8;
             let ig = (255.99*col.g()) as u8;
             let ib = (255.99*col.b()) as u8;
@@ -96,15 +99,36 @@ fn color<T: Hitable>(r: &geometric::Ray, world: &T) -> Vec3 {
     //let unit_direction = unit_vector(&r.direction);
     // let t =  hit_sphere(&Vec3(0.0, 0.0, -1.0), 0.5, r);
     let mut rec = HitRecord{t: 0.0, p: Vec3(0.0,0.0,0.0), normal: Vec3(0.0,0.0,0.0)};
-    if world.hit(r, 0.0, f64::INFINITY, &mut rec)
+    if world.hit(r, 0.001, f64::INFINITY, &mut rec)
     {
-        return Vec3(rec.normal.x() + 1.0, rec.normal.y() + 1.0, rec.normal.z() + 1.0)*0.5;
+        let target = &rec.p + &rec.normal + random_in_unit_sphere();
+        let ray = Ray {
+            origin: rec.p.clone(), 
+            direction: target - &rec.p
+        };
+        return color(&ray, world)*0.5;
+        //return Vec3(rec.normal.x() + 1.0, rec.normal.y() + 1.0, rec.normal.z() + 1.0)*0.5;
     }
     else {
         let unit_direction = r.direction.normalize();
         let t = (unit_direction.y() + 1.0)*0.5;
         return Vec3(1.0, 1.0, 1.0)*(1.0 - t) + Vec3(0.5, 0.7, 1.0)*t;
     }
+}
+
+fn random_in_unit_sphere() -> Vec3 {
+    let mut p = Vec3 (0.0, 0.0, 0.0);
+    let mut rng = rand::rng();
+    loop {
+        let x : f64 = rng.random();
+        let y : f64 = rng.random();
+        let z : f64 = rng.random();
+        p = Vec3(x, y, z)*2.0 - Vec3(1.0, 1.0, 1.0);
+        if (p.length()*p.length() < 1.0) {
+            break;
+        }
+    }
+    p
 }
 
 
